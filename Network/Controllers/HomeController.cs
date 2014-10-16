fd2beb4ee0cf883796f8c8677eaf9c30d13e51ee
dolphan <emilvik@kth.se>
2014-10-16 10:38:50 +0200

    OK!

--- Network/Controllers/HomeController.cs
+++ Network/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace Network.Controllers
 {
  
-    //Hello World
+    //Hello World OK!
     public class HomeController : Controller
     {
         public ActionResult Index()
