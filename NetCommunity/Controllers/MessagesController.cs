e35a89da1c1c50dcb3c4e266fb0fb26140c36a23
Urbansson <Teddy@brandts.se>
2014-10-20 18:04:34 +0200

    Added Messages

---
+++ NetCommunity/Controllers/MessagesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NetCommunity.Models;
+
+namespace NetCommunity.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.Reciver).Include(m => m.Sender);
+            return View(messages.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReciverId = new SelectList(db.Users, "Id", "Email");
+            ViewBag.SenderId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SenderId,ReciverId,Title,Content,IsRead,Time")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReciverId = new SelectList(db.Users, "Id", "Email", message.ReciverId);
+
+            ViewBag.SenderId = new SelectList(db.Users, "Id", "Email", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReciverId = new SelectList(db.Users, "Id", "Email", message.ReciverId);
+            ViewBag.SenderId = new SelectList(db.Users, "Id", "Email", message.SenderId);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SenderId,ReciverId,Title,Content,IsRead,Time")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReciverId = new SelectList(db.Users, "Id", "Email", message.ReciverId);
+            ViewBag.SenderId = new SelectList(db.Users, "Id", "Email", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
