7216a8eb660f1f71ddb27794104a3885956e38c9
Urbansson <Teddy@brandts.se>
2014-10-22 13:37:55 +0200

    Added Groups

---
+++ NetCommunity/Migrations/201410221122351_Groups3.cs
@@ -0,0 +1,16 @@
+namespace NetCommunity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Groups3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
